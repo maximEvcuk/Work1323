e584fe71593597daba152f5d9965733d7fd362fe
Maks <maks35@gmail.com>
2025-01-18 19:33:16 +0200

    ~Reworked~

--- ~Work1323/Work2.cpp
+++ ~Work1323/Work2.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
 
 class Shape {
 public:
 	virtual void Show() const = 0;
-	virtual void Save(std::ofstream &out) const = 0;
-	virtual void Load(std::ifstream &in) = 0;
+	virtual std::string Save() const = 0;
+	virtual void Load(const std::string &data) = 0;
 	virtual ~Shape() {}
 };
 
@@ -19,15 +18,15 @@ public:
 	Square(int x, int y, int side) : x(x), y(y), side(side) {}
 
 	void Show() const override {
-		std::cout << "Square: Top-left(" << x << ", " << y << "), Side lenght: " << side << std::endl;
+		std::cout << "Square: Top-left(" << x << ", " << y << "), Side length: " << side << std::endl;
 	}
 
-	void Save(std::ofstream &out) const override {
-		out << "Square " << x << " " << y << " " << side << std::endl;
+	std::string Save() const override {
+		return "Square " + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(side);
 	}
 
-void Load(std::ifstream &in) override {
-	in  >> x >>  y >>  side;
+   void Load(const std::string& data) override {
+	   sscanf(data.c_str(), "Square %d %d %d ", &x, &y, &side);
    }
 };
 
@@ -39,15 +38,15 @@ public:
 		Rectangle(int x, int y, int width, int height) : x(x), y(y), width(width), height(height){}
 
 		void Show() const override {
-			std::cout << "Rectangle: Top-left(" << x << ", " << y << "), Width: " << width << ", Height: "<< height << std::endl;
+			std::cout << "Rectangle: Top-left(" << x << ", " << y << "), Width: " << width << ", Height: " << height << std::endl;
 		}
 
-		void Save(std::ofstream &out) const override {
-			out << "Rectangle " << x << " " << y << " " << width << " " << height << std::endl;
+		std::string Save() const override {
+			return "Rectangle" + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(width) + " " + std::to_string(height);
 		}
 
-		void Load(std::ifstream &in) override {
-			in >> x >> y >> width >> height;
+		void Load( const std::string &data) override {
+			sscanf(data.c_str(), "Rectangle %d %d %d %d ", &x, &y, &width, &height);
 		}
 };
 
@@ -62,12 +61,13 @@ public:
 		std::cout << "Circle: Center (" << centerX << ", " << centerY << "), Radius: " << radius <<  std::endl;
 	}
 
-	void Save(std::ofstream &out) const override {
-		out << "Circle " << centerX << " " << centerY << " " << radius << std::endl;
+	std::string Save() const override {
+		 return "Circle " + std::to_string(centerX) +" " + std::to_string(centerY) + " " + std::to_string(radius);
 	}
 
-	void Load(std::ifstream &in) override {
-		in >> centerX >> centerY >> radius;
+	void Load(const std::string& data) override {
+		sscanf(data.c_str(), "Circle %d %d %d  ", &centerX, &centerY, &radius);
+
 	}
 
 };
@@ -83,17 +83,17 @@ public:
         std::cout << "Ellipse: Top-left (" << x << ", " << y << "), Width: " << width << ", Height: " << height << std::endl;
     }
 
-    void Save(std::ofstream &out) const override {
-        out << "Ellipse " << x << " " << y << " " << width << " " << height << std::endl;
-    }
+	std::string Save() const override {
+		return "Ellipse " + std::to_string(x) + " " + std::to_string(y) + " " + std::to_string(width), std::to_string(height);
+	}
 
-    void Load(std::ifstream &in) override {
-        in >> x >> y >> width >> height;
-    }
+		void Load(const std::string & data) override {
+			sscanf(data.c_str(), "Ellipse %d %d %d %d ", &x, &y, &width, &height);
+
+		}
 };
 
 int main() {
-
 	std::vector<Shape*> shapes;
 
 	shapes.push_back(new Square(10, 10, 5));
@@ -101,40 +101,34 @@ int main() {
 	shapes.push_back(new Circle(30, 30, 7));
 	shapes.push_back(new Ellipse(40, 40, 8, 5));
 
-	std::ofstream outFile("shapes.txt");
+	std::vector<std::string> savedShapes;
 	for (const auto& shape : shapes) {
-		shape->Save(outFile);
+		savedShapes.push_back(shape->Save());
 	}
-	outFile.close();
 
 	std::vector<Shape*> loadedShapes;
-	std::ifstream inFile("shapes.txt");
-	std::string shapeType;
-
-	while (inFile >> shapeType) {
-		if (shapeType == "Square") {
+	for (const auto& shapeData : savedShapes) {
+		if (shapeData.find("Square") == 0) {
 			Square* square = new Square(0, 0, 0);
-			square->Load(inFile);
+			square->Load(shapeData);
 			loadedShapes.push_back(square);
 		}
-		else if (shapeType == "Rectangle") {
-			Rectangle*
-				rectangle = new Rectangle(0, 0, 0, 0);
-			rectangle->Load(inFile);
+		else if (shapeData.find("Rectangle") == 0) {
+			Rectangle* rectangle = new Rectangle(0, 0, 0, 0);
+			rectangle->Load(shapeData);
 			loadedShapes.push_back(rectangle);
 		}
-		else if (shapeType == "Circle") {
+		else if (shapeData.find("Circle") == 0) {
 			Circle* circle = new Circle(0, 0, 0);
-			circle->Load(inFile);
+			circle->Load(shapeData);
 			loadedShapes.push_back(circle);
 		}
-		else if (shapeType == "Ellipse") {
+		else if (shapeData.find("Ellipse") == 0) {
 			Ellipse* ellipse = new Ellipse(0, 0, 0, 0);
-			ellipse->Load(inFile);
+			ellipse->Load(shapeData);
 			loadedShapes.push_back(ellipse);
 		}
 	}
-	inFile.close();
 
 	for (const auto& shape : loadedShapes) {
 		shape->Show();
@@ -147,4 +141,7 @@ int main() {
 
 	return 0;
 }
+
+	
+
 
